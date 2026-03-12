5965d993d6f92facac965685d113e64496c6ecbb
Sergio andres tiria lancheros <osorioemmanuel79@gmail.com>
2026-03-12 11:56:50 -0500

    fun1

---
+++ functions.py
@@ -0,0 +1,9 @@
+def promedio_estudiante(calificaciones):
+    if len(calificaciones)== 0:
+        return 0.0
+    else:
+        resultado = sum(calificaciones) / len(calificaciones)
+        return float(resultado)
+calificaciones=[]
+print(promedio_estudiante(calificaciones))
+    
